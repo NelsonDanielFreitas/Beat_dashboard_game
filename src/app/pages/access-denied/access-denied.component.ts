003da28d93badf9192cde2441d30b5eb542f3dd8
NelsonDanielFreitas <nelsonfreitas@ipvc.pt>
2025-03-04 11:40:27 +0000

    Fix: Foi corrigido para aparecer os labels nas caixas de input

--- src/app/pages/access-denied/access-denied.component.ts
+++ src/app/pages/access-denied/access-denied.component.ts
@@ -4,36 +4,38 @@ import { GlobalDecorComponent } from '../../global-decor/global-decor.component'
 @Component({
   selector: 'app-access-denied',
   template: `
-      <div class="access-denied">
-        <h1>Acesso não autorizado!</h1>
-        <p>Não tem permissões para aceder a esta página.</p>
-        <a href="/login">Voltar ao login</a>
-      </div>
-    `,
-  styles: [`
-    .access-denied {
-      width: 100vw;
-      height: 100vh;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      flex-direction: column;
-    }
-    .access-denied h1 {
-        color: #1A4157;
+    <div class="access-denied">
+      <h1>Unauthorised access!</h1>
+      <p>You don't have permissions to access this page.</p>
+      <a href="/login">Back to login</a>
+    </div>
+  `,
+  styles: [
+    `
+      .access-denied {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+      }
+      .access-denied h1 {
+        color: #1a4157;
         text-align: center;
-    }
-    .access-denied p {
-      color: red;
-    }
-    .access-denied a {
-      font-size: 24px;
-      margin-top: 16px;
-      color: #1A4157;
-    }
-    .access-denied a:hover {
-      color: #72AA45;
-    }
-  `]
+      }
+      .access-denied p {
+        color: red;
+      }
+      .access-denied a {
+        font-size: 24px;
+        margin-top: 16px;
+        color: #1a4157;
+      }
+      .access-denied a:hover {
+        color: #72aa45;
+      }
+    `,
+  ],
 })
 export class AccessDeniedComponent {}
